0f05b26d9560b1bc8f9006b95241679e5d7c3094
James Kelleher <jk3047@nyu.edu>
2015-04-05 22:56:23 -0400

    finished basic code, still need to comment more

--- seed.py
+++ seed.py
@@ -1,18 +1,85 @@
-import requests
 import json
 
-api_key = 'ed6cd1dd2b34712cbf3018387d28332b'
-# api_secret = 'c24df07c9de358b09d0991fd623df366'
+import pandas as pd
+import numpy as np
 
+from requests import get
+from pandas import Series, DataFrame
+
+api_key = 'ed6cd1dd2b34712cbf3018387d28332b'
+
+# endpoint
 url = 'http://ws.audioscrobbler.com/2.0/'
 
-resp = requests.get(url, params={'method': 'chart.getTopArtists',
-                                 'api_key': api_key,
-                                 'format': 'json',
-                                 'limit': 1000})
-                                 
-artists = json.loads(resp.text)['artists']['artist']
+# parameters needed to use the 'chart.getTopArtists' method
+params = {'method': 'chart.getTopArtists', 'api_key': api_key, 'format': 'json', 'limit': 1000}
 
-params = {'method': }
+# IMPORTANT NOTE: in my personal testing, for a while the 'chart.getTopArtists' method failed
+# to return information. this was most likely an issue on last.fm's side. if the issue persists,
+# uncommenting the following line will allow you to test the code using 'chart.getHypedArtists'
+
+# params['method'] = 'chart.getHypedArtists'
+
+# a list of all necessary artist info
+artists = get(url, params=params).json()['artists']['artist']
+            
+# will be used to construct a DataFrame the relates artists to tag counts                     
+tag_series = []
+
+# parameters needed to use the 'artist.getTopTags' method
+params = {'method': 'artist.getTopTags', 'api_key': api_key, 'format':'json'}
+
 for artist in artists:
-    artist_tags = requests.get(url, params=)
+    
+    # we add the artist to the parameters
+    params['artist'] = artist['name']
+    
+    tags = get(url, params=params).json()['toptags']
+    
+    # check for the possibility of no tags
+    if 'tag' in tags:
+        tags = tags['tag']
+    else:
+        continue
+        
+    # if there is only one tag, 'tags' is a dict, instead of a list
+    # we need to make sure 'tags' is always a list
+    if isinstance(tags, dict):
+        tags = [tags]
+        
+    # tag_info is used to build the Series that we in turn use to build the DataFrame
+    tag_info = {}
+    for tag in tags:
+        # it is my understanding that when a tag has 'count' = 0, only one user submitted the
+        # tag (otherwise, there simply wouldn't be a tag). therefore, 'count' = 0 really means a 
+        # single tag. it follows that all counts should be incremented by 1, given that last.fm
+        # apparently begins its count at 0
+        tag_info[tag['name']] = int(tag['count']) + 1
+        
+    
+    # construct a series indexed on tag counts
+    s = Series(tag_info, name=artist['name'])
+    
+    # add the series to our list
+    tag_series.append(s)
+
+# create the DataFrame. since 'count' = 0 really means 'count' = 1, missing info can therefore
+# be filled as 0
+tag_frame = DataFrame(tag_series).fillna(0)
+
+def tag_association(t1, t2, df=tag_frame):
+    v1, v2 = df[t1], df[t2]
+    dot = np.dot(v1, v2)
+    norm_prod = np.linalg.norm(v1) * np.linalg.norm(v2)
+    cos_sim = dot / norm_prod
+    return cos_sim
+    
+rh = tag_association('rap', 'hip hop')
+rr = tag_association('rap', 'rock')
+rc = tag_association('rap', 'country')
+
+print 'Similarity between rap and hip-hop: ' + rh
+print 'Similarity between rap and rock: ' + rr
+print 'Similarity between rap and country: ' + rc
+
+    
